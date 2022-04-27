ec8583a46cfd3fa09593190aac59706c5cb020c7
jasu62 <jasu62@mailbox.org>
2022-04-26 19:06:18 -0500

    Implemented all required functionality.
    Navigating to /api/whoami results in the app responding with a JSON
    object containing the user's IP address and accept-language and
    user-agent strings.
    
    The style of the comments originally in the file was also changed.
    
    The .gitignore file was updated to include the .DS_Store files prevalent
    on Mac machines.

--- server.js
+++ server.js
@@ -1,33 +1,50 @@
-// server.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// Enable cross-origin resource sharing
+// so that the API can be remotely tested by FCC
+const cors = require('cors');
+// Some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
-// http://expressjs.com/en/starter/static-files.html
+// Declare location of static assets (CSS, JS, images)
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+
+// Route requests for root to the index page
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// The first API endpoint
+app.get('/api/hello', function (req, res) {
+  res.json({greeting: 'Hello, world!'});
 });
 
 
+/*
+When user navigates to /api/whoami,
+the server will return an object with the following:
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+ipaddress: 159.20.14.100
+language:  en-US,en;q=0.5
+software:  Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0
+
+(Field values are provided as an example.)
+*/
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
+});
+
+
+// Start server and listen for requests
+const port = ('PORT' in process.env) ? process.env.PORT : 3000;
+app.listen(port, () => {
+  console.log(`The app is listening on port ${port}.`);
 });
